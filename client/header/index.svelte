74a3774367d2d43c96e9956599e770f8aaa0b8d2
Tomasz Borychowski <tborychowski@gmail.com>
2020-04-25 10:21:20 +0100

    expandable services

--- client/header/index.svelte
+++ client/header/index.svelte
@@ -2,13 +2,11 @@
 	<h1><em>{$system.hostname || ''}</em></h1>
 	<div class="updated-at">updated at: {$updatedAt || ''}</div>
 		<!-- <small>({$system.hostip})</small> -->
-	<Summary/>
 </header>
 
 
 <script>
 import {system, updatedAt} from '../store';
-import Summary from './summary';
 
 
 </script>
--- client/services/index.svelte
+++ client/services/index.svelte
@@ -1,12 +1,22 @@
-<div class="panel">
+<div class="panel services-panel">
 	<div class="content">
-		{#each $services as service}
-			<ServiceBox service="{service}"/>
-		{/each}
+		<header class:expanded="{expanded}"
+			on:click="{() => expanded = !expanded}">
+			<h1>Services</h1>
+			<Summary/>
+		</header>
+		<div class="services-list" class:expanded="{expanded}">
+			{#each $services as service}
+				<ServiceBox service="{service}"/>
+			{/each}
+		</div>
 	</div>
 </div>
 
 <script>
 import ServiceBox from './service-box';
+import Summary from './summary';
 import {services} from '../store';
+
+let expanded = false;
 </script>
