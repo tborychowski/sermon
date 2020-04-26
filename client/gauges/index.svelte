691ed81d6253728622b278f629760c3752b9b061
Tomasz Borychowski <tborychowski@gmail.com>
2020-04-26 15:22:46 +0100

    cln; transitions

--- client/gauges/index.svelte
+++ client/gauges/index.svelte
@@ -1,25 +1,23 @@
-<div class="panel">
-	<div class="content gauges-content">
-		<Gauge
-			size="S"
-			label="Temperature"
-			value="{$system.temp}"
-			unit="°C"
-			warn="40"
-			alert="60"
-		/>
-		<Gauge
-			size="M"
-			label="RAM"
-			value="{parseFloat($system.memUsed) || 0}"
-			max="{parseFloat($system.memTotal) || 32}"
-			ticks="0,4,8,12,16,24,28,32"
-			warn="24"
-			alert="28"/>
-		<Gauge label="1 min" value="{$system.load[0]}" size="L"/>
-		<Gauge label="5 min" value="{$system.load[1]}" size="M"/>
-		<Gauge label="15 min" value="{$system.load[2]}" size="S"/>
-	</div>
+<div class="panel panel-gauges">
+	<Gauge
+		size="S"
+		label="Temperature"
+		value="{$system.temp}"
+		unit="°C"
+		warn="40"
+		alert="60"
+	/>
+	<Gauge
+		size="M"
+		label="RAM"
+		value="{parseFloat($system.memUsed) || 0}"
+		max="{parseFloat($system.memTotal) || 32}"
+		ticks="0,4,8,12,16,24,28,32"
+		warn="24"
+		alert="28"/>
+	<Gauge label="1 min" value="{$system.load[0]}" size="L"/>
+	<Gauge label="5 min" value="{$system.load[1]}" size="M"/>
+	<Gauge label="15 min" value="{$system.load[2]}" size="S"/>
 </div>
 
 <script>
--- client/services/index.svelte
+++ client/services/index.svelte
@@ -1,19 +1,23 @@
-<div class="panel services-panel">
-	<div class="content">
-		<header class:expanded="{expanded}"
-			on:click="{() => expanded = !expanded}">
-			<h1>Services</h1>
-			<Summary/>
-		</header>
-		<div class="services-list" class:expanded="{expanded}">
+<div class="panel panel-services">
+
+	<header class:expanded="{expanded}"
+		on:click="{() => expanded = !expanded}">
+		<h1>Services</h1>
+		<Summary/>
+	</header>
+
+	{#if expanded}
+		<div class="services-list" transition:slide>
 			{#each $services as service}
 				<ServiceBox service="{service}"/>
 			{/each}
 		</div>
-	</div>
+	{/if}
+
 </div>
 
 <script>
+import {slide} from 'svelte/transition';
 import ServiceBox from './service-box';
 import Summary from './summary';
 import {services} from '../store';
